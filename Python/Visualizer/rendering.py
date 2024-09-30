edb0cc0947052c3735d449eb0e2a0715a2af9c8e
Alfonso López Ruiz <allopezr@ujaen.es>
2024-09-30 17:25:55 +0200

    Remove unused folders

--- Python/Visualizer/rendering.py
+++
@@ -1,6 +0,0 @@
-import matplotlib.pyplot as plt
-
-
-def set_font(name='Adobe Devanagari', size=14):
-    font_mapping = {'family': name, 'weight': 'normal', 'size': size}
-    plt.rc('font', **font_mapping)
